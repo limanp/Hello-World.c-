749157a699522003a9397fd4b60b1e0cff02a88c
moviflow <joblimanaleksej@gmail.com>
2022-02-12 22:36:44 +0200

    first commit

---
+++ Hello-World/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет Мир!");
+            Console.WriteLine("Это моя первая программа на языке [c# за решеткой]");
+            Console.WriteLine("version <0.00000000001>");
+        }
+    }
+}
